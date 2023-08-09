29d4875089edcd2103404041e408023931e7a3fc
Uros Terzic <urosterzicbiz@gmail.com>
2023-08-09 18:47:31 +0200

    Added save and display tasks functions..

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ function addTask() {
         let span = document.createElement('span');
         span.innerHTML = '\u00d7';
         li.appendChild(span);
+        saveData();
     }
 
     inputBox.value = '';
@@ -20,7 +21,19 @@ listContainer.addEventListener("click", (e) => {
     console.log(e.target.tagName);
     if (e.target.tagName === "LI") {
         e.target.classList.toggle('checked');
+        saveData();
     } else if (e.target.tagName === 'SPAN') {
         e.target.parentElement.remove();
+        saveData();
     }
 }, false)
+
+function saveData() {
+    localStorage.setItem('data', listContainer.innerHTML);
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem('data');
+}
+
+showTask();
