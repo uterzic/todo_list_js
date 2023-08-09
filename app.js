49cfe87b6c93f585c4810f3c0994cadaf5539b5e
Uros Terzic <urosterzicbiz@gmail.com>
2023-08-09 18:40:09 +0200

    Added funcionality to delete an item from the list.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const inputBox = document.getElementById('input-box');
-const listContainer = document.querySelector('.list-container');
+const listContainer = document.getElementById('list-container');
 
 function addTask() {
     if(inputBox.value === '') {
@@ -8,7 +8,19 @@ function addTask() {
         let li = document.createElement('li');
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = '\u00d7';
+        li.appendChild(span);
     }
 
     inputBox.value = '';
 }
+
+listContainer.addEventListener("click", (e) => {
+    console.log(e.target.tagName);
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle('checked');
+    } else if (e.target.tagName === 'SPAN') {
+        e.target.parentElement.remove();
+    }
+}, false)
